33d3855ed508e88d0c4c122363d653b01bd5ad2a
justusjb <jujo03h@icloud.com>
2024-03-21 16:46:46 +0100

    simplified expression to increase compatibility

--- scripts/grml.sh
+++ scripts/grml.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 option() {
-    if [[ ${1,,} == @(true|on) ]]; then
+    local lower_option="${1,,}"  # Convert to lowercase
+    if [[ "$lower_option" == "true" ]] || [[ "$lower_option" == "on" ]]; then
         echo "$2"
     else
         echo "$3"
     fi
 }
+
 
 skipBinaries() {
     if [[ "$(option ${skipBinaries} 'true')" == "true" ]]; then
