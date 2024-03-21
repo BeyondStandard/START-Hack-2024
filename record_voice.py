3abed4b9cd520b5ce014dcd2aab3f04066bebe47
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2024-03-21 20:41:10 +0100

    save audio file

--- record_voice.py
+++ record_voice.py
@@ -1,4 +1,5 @@
 import asyncio
+import wave
 from array import array
 import sounddevice as sd
 from scipy.io.wavfile import write
@@ -11,11 +12,17 @@ MIN_VOLUME = 2600
 BUF_MAX_SIZE = CHUNK_SIZE * 10
 q = asyncio.Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK_SIZE)))
 stop_event = asyncio.Event()
+wf = wave.open("output.wav", "wb")
+wf.setnchannels(2)
+wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paInt16))
+wf.setframerate(44100)
+
 
 async def main():
     listener_task = asyncio.create_task(listen(q))
     recorder_task = asyncio.create_task(record(q))
     await asyncio.gather(listener_task, recorder_task)
+
 
 async def record(q):
     pause_timer = 1
@@ -28,13 +35,15 @@ async def record(q):
             pause_timer = pause_timer + 1
             if pause_timer == 50:
                 print("break now")
-                #os.system('python runner.py')
+                wf.close()
+                # os.system('python runner.py')
                 stop_event.set()
+
 
 async def listen(q):
     stream = pyaudio.PyAudio().open(
         format=pyaudio.paInt16,
-        channels=1,
+        channels=2,
         rate=44100,
         input=True,
         frames_per_buffer=1024,
@@ -45,6 +54,7 @@ async def listen(q):
             await q.put(array('h', stream.read(CHUNK_SIZE)))
         except asyncio.queues.QueueFull:
             return  # discard
+
 
 if __name__ == '__main__':
     asyncio.run(main())
