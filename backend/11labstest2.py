6eabcc594ab732110df77b9d19b4930ff324b49a
justusjb <jujo03h@icloud.com>
2024-03-21 01:19:22 +0100

    added eleven labs access

---
+++ backend/11labstest2.py
@@ -0,0 +1,23 @@
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import datetime as dt
+
+
+start = dt.datetime.now()
+
+client = ElevenLabs(
+  api_key="41f1d61b1ce48269216086555aa78d33",
+)
+
+
+audio = client.generate(
+    text="Weiß ich nicht, lass mal wieder gehen und Bäume fällen.",
+    voice="Chris",
+    model='eleven_multilingual_v1'
+)
+
+end = dt.datetime.now()
+
+print("Time elapsed for generating:", end - start)
+
+play(audio)
