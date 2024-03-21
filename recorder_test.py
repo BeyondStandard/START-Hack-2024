bab62d9f9b1c6ebbd6a21b75a9ca271e996ad9e6
nshreyasvi <shreyasvi.natraj@etu.unige.ch>
2024-03-21 20:17:29 +0100

    errors

--- recorder_test.py
+++ recorder_test.py
@@ -17,26 +17,38 @@ def main():
     stopped = threading.Event()
     q = Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK_SIZE)))
 
-    listen_t = threading.Thread(target=listen, args=(stopped, q))
-    listen_t.start()
+    #listen_t = threading.Thread(target=listen, args=(stopped, q))
+    #listen_t.start()
     record_t = threading.Thread(target=record, args=(stopped, q))
     record_t.start()
 
     try:
         while True:
-            listen_t.join(0.1)
+            #listen_t.join(0.1)
             record_t.join(0.1)
     except KeyboardInterrupt:
         stopped.set()
 
-    listen_t.join()
+    #listen_t.join()
     record_t.join()
 
 def record(stopped, q):
     pause_timer = 1
+    stream = pyaudio.PyAudio().open(
+        format=pyaudio.paInt16,
+        channels=2,
+        rate=44100,
+        input=True,
+        frames_per_buffer=1024,
+    )
     while True:
         if stopped.wait(timeout=0):
             break
+        try:
+            q.put(array('h', stream.read(CHUNK_SIZE)))
+        except Full:
+            break
+
         chunk = q.get()
         vol = max(chunk)
         #print(vol)
@@ -52,25 +64,24 @@ def record(stopped, q):
             if pause_timer == 50:
                 print("break now")
                 #os.system('python runner.py')
-                continue
-            #return
+                break
 
-def listen(stopped, q):
-    stream = pyaudio.PyAudio().open(
-        format=pyaudio.paInt16,
-        channels=2,
-        rate=44100,
-        input=True,
-        frames_per_buffer=1024,
-    )
+#def listen(stopped, q):
+#    stream = pyaudio.PyAudio().open(
+#        format=pyaudio.paInt16,
+#        channels=2,
+#        rate=44100,
+#        input=True,
+#        frames_per_buffer=1024,
+#    )
 
-    while True:
+#    while True:
         #if stopped.wait(timeout=0):
         #    break
-        try:
-            q.put(array('h', stream.read(CHUNK_SIZE)))
-        except Full:
-            break  # discard
+#        try:
+#            q.put(array('h', stream.read(CHUNK_SIZE)))
+#        except Full:
+#            break  # discard
 
 
 if __name__ == '__main__':
