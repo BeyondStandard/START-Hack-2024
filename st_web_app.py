87ee32b54078fac199c6b26e02c9da3a15906cf9
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2024-03-22 00:26:11 +0100

    Refactor

--- st_web_app.py
+++ st_web_app.py
@@ -1,25 +1,24 @@
-import io
 import os
-import streamlit as st
-import pandas as pd
-import numpy as np
 
-col1_1, col2_1, col3_1 = st.columns([1.5,5,1]) 
+import streamlit as st
+
+col1_1, col2_1, col3_1 = st.columns([1.5, 5, 1])
 
 col2_1.title('Team Beyond Standard')
-st.image(os.path.join('pics','hack.png'))
-st.image(os.path.join('pics','st_gallen.jpg'))
+st.image(os.path.join('pics', 'hack.png'))
+st.image(os.path.join('pics', 'st_gallen.jpg'))
 
-#Launch Button
-col1, col2, col3 = st.columns([1.5,1,1]) 
+# Launch Button
+col1, col2, col3 = st.columns([1.5, 1, 1])
 
-#img = Image.open("images/top_spiderman.png")
-#st.button(st.image(img))
+# img = Image.open("images/top_spiderman.png")
+# st.button(st.image(img))
 
-if col2.button('Ask a qusetion!'):
+if col2.button('Ask a question!'):
+    col2.write("Listening...")
     os.system('python record_voice.py')
 
     # If you're curious of all the loggers
 
-    #chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-    #st.bar_chart(chart_data)
+    # chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+    # st.bar_chart(chart_data)
