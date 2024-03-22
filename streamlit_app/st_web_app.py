70e0bdadb0218b60f36e269733242f5965b940da
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2024-03-22 01:13:31 +0100

    streamlit

--- streamlit_app/st_web_app.py
+++ streamlit_app/st_web_app.py
@@ -1,5 +1,7 @@
+import json
 import os
-
+import pandas as pd
+import numpy as np
 import streamlit as st
 
 col1_1, col2_1, col3_1 = st.columns([1.5, 5, 1])
@@ -18,7 +20,8 @@ if col2.button('Ask a question!'):
     col2.write("Listening...")
     os.system('python record_voice.py')
 
-    # If you're curious of all the loggers
-
-    # chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-    # st.bar_chart(chart_data)
+if col3.button('View history'):
+    df = pd.read_json("streamlit_app/ratings.json")
+    df["sentiment"] = df["sentiment"].astype("category")
+    df["emotion"] = df["emotion"].astype("category")
+    st.bar_chart(df, x="timestamp", y="emotion", color="sentiment")
